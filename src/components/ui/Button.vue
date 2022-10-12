bd5aa78b7b004455885d3c943605423d4596a3f4
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-10-11 23:12:55 -0500

    feat: add Button component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,19 @@
+<script setup>
+const styles = {
+  white:
+    'block w-full rounded-lg bg-white text-black font-medium py-2 hover:bg-gray-100',
+}
+
+const props = defineProps({
+  variant: {
+    type: String,
+    validators: (value) => ['primary', 'white'].includes(value),
+  },
+})
+</script>
+
+<template>
+  <button :class="styles[props.variant]">
+    <slot />
+  </button>
+</template>
