2785827bb00cf9a137441d3460401e56ad98eb02
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-11-02 14:03:01 -0500

    refactor: remove navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav class="p-4">
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
   <RouterView />
 </template>
