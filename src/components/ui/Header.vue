9f688f6dd53f7687adc3d3a684ab86ebce607434
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-11-02 14:01:16 -0500

    feat: add ui components

---
+++ src/components/ui/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { TheLogo } from '@/components/ui'
+</script>
+
+<template>
+  <nav class="p-4 flex items-center bg-white">
+    <div class="flex gap-2 items-center">
+      <TheLogo class="h-8 w-8" />
+      <span class="font-bold text-xl text-gray-900 tracking-wide">BANKR</span>
+    </div>
+    <RouterLink to="/">Go Home</RouterLink>
+  </nav>
+</template>
---
+++ src/components/ui/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="sticky top-0 z-50">
+    <slot name="header"></slot>
+  </header>
+  <main class="relative flex flex-col overflow-hidden">
+    <aside></aside>
+    <slot></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
