221d771ab291e23dc7a60365cf115b7cf34d5e4b
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-11-02 14:44:35 -0500

    wip: auth view with login method

--- src/auth/views/AuthView.vue
+++ src/auth/views/AuthView.vue
@@ -1,52 +1,44 @@
 <script setup>
-  import { 
-    Logo,
-    TextField,
-    PasswordField,
-    LinkButton,
-    Button,
-    Tooltip,
-  } from '../../components';
+import {
+  Logo,
+  TextField,
+  PasswordField,
+  LinkButton,
+  Button,
+  Tooltip,
+} from '../../components'
+import { ref } from 'vue'
+import { api } from '../../main'
+
+let form = ref({ email: '', password: '' })
+
+async function login() {
+  let response = await api.instance.post('/login', { body: form.value })
+  console.log(response)
+}
 </script>
 
 <template>
-
-  <article class='flex'>
-    <section 
-      class='w-1/2 h-screen bg-primary flex flex-col justify-center items-center'
+  <article class="flex">
+    <section
+      class="w-1/2 h-screen bg-primary flex flex-col justify-center items-center"
     >
-      <img 
-        src='../../assets/auth_view_img.png' 
-        class='w-[500px]'
-      />
+      <img src="../../assets/auth_view_img.png" class="w-[500px]" />
 
-      <div
-        class='w-96 text-center'
-      >
-        <h4
-          class='text-3xl font-bold mt-12'
-        >
+      <div class="w-96 text-center">
+        <h4 class="text-3xl font-bold mt-12">
           Reciba pagos de cualquier sistema bancario
         </h4>
-        <p
-          class='mt-8 text-lg font-light'
-        >
+        <p class="mt-8 text-lg font-light">
           Conecte su tarjeta bancaria y cree cuentas en la moneda seleccionada.
         </p>
       </div>
-
     </section>
-    <section 
-      class='w-1/2 h-screen px-32 flex flex-col justify-center'
-    >
+    <section class="w-1/2 h-screen px-32 flex flex-col justify-center">
       <Logo />
-      <h2 class='font-semibold text-4xl my-5'>
-        Bienvenido
-      </h2>
-      <p class='mb-6'>
-        Inicie sesión para acceder a su cuenta.
-      </p>
-      
+      <h2 class="font-semibold text-4xl my-5">Bienvenido</h2>
+      <p class="mb-6">Inicie sesión para acceder a su cuenta.</p>
+
       <!--
         TODO: 
         - mover este formulario a otro componente FormLogin
@@ -54,45 +46,39 @@
         - implementar el cambio entre un formulario y otro
       -->
 
-      <form
-        class='flex flex-col justify-center'
-      >
-        <TextField
-          label='Correo electronico'
-          value=''
-          placeholder='Escriba su correo electronico'
-          info='necesitamos tu correo'
-        />
-        
-        <br/>
-
-        <PasswordField
-          label='Password'
-          value=''
-          placeholder='Escriba su contraseña'
+      <div class="flex flex-col justify-center">
+        <label class="text-gray-600 text-lg mb-2" for="email">Email</label>
+        <input
+          class="bg-slate-100 px-4 py-2 rounded-md"
+          type="email"
+          v-model="form.email"
         />
 
-        <LinkButton
-          class='mt-4'
-          content='¿Has olvidado tu contraseña?'
-        />
-        
-        <Button
-          class='mt-12'
-          content='Iniciar sesión'
-        />
-      </form>
+        <br />
 
-      <div 
-        class='text-center mt-4'
-      >
-        ¿No tienes una cuenta?
-        <LinkButton 
-          content='Registrarme'
+        <label class="text-gray-600 text-lg mb-2" for="password"
+          >Contraseña</label
+        >
+        <input
+          class="bg-slate-100 px-4 py-2 rounded-md"
+          type="password"
+          v-model="form.password"
         />
+
+        <LinkButton class="mt-4" content="¿Has olvidado tu contraseña?" />
+
+        <button
+          class="bg-primary text-white text-xl py-3 rounded-md mt-12"
+          @click="login"
+        >
+          Iniciar sesión
+        </button>
       </div>
-      
 
+      <div class="text-center mt-4">
+        ¿No tienes una cuenta?
+        <LinkButton content="Registrarme" />
+      </div>
     </section>
   </article>
 </template>
