8fb47a8426d49e16207767d6df2ebb47fb213673
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-11-16 10:23:05 -0500

    feat: add vue-toastification

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import './assets/main.css'
 import API from './api'
 
@@ -9,5 +11,7 @@ const app = createApp(App)
 export const api = new API()
 
 app.use(router)
+
+app.use(Toast)
 
 app.mount('#app')
