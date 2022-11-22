622d703ec43d105aabfaa3dbef80515b8b347a05
alex mendoza <alexdylam2016@gmail.com>
2022-11-21 21:16:56 -0500

    File Detail transaction

--- src/views/external-transaction/Detail-transaction.vue
+++ src/views/external-transaction/Detail-transaction.vue
@@ -1,10 +1,17 @@
 <script></script>
 <template>
+      
       <Navbar></Navbar>
   
   <div class="w-full">
     <section class="container mx-auto py-3 space-y-4">
-        
+        <h2>Tipo de transacción</h2>
+        <h2>tipo de cuenta</h2>
+        <h2>Monto</h2>
+        <h2>Nombre del propietario de la Cuenta</h2>
+        <h2>Nombre de Banco</h2>
+        <h2>Estado de la transaccion</h2>
+        <h2>Descripcion</h2>
     </section>
     </div>
 </template>
